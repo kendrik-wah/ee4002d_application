a5532e565af029898410890f0b09a921ff61a805
kendrik-wah <kendrik_wah@yahoo.com.sg>
2021-01-22 18:06:55 +0800

    added test cases and updated demo script

---
+++ algorithms/__init__.py
@@ -0,0 +1,43 @@
+import math
+
+four_sided_dirs = ((0, -1), (0, 1), (1, 0), (-1, 0))
+eight_sided_dirs = ((0, -1), (0, 1), (1, 0), (-1, 0), (-1,-1), (-1,1), (1,-1), (1,1))
+
+
+
+def get_clustered_points(data):
+
+    m = len(data)
+    n = len(data[0])
+
+    seen = set()
+    clusters = dict()
+
+    for i in range(m):
+        for j in range(n):
+
+            if data[i][j] != 1:
+
+                points = set()
+                idx = data[i][j]
+                stk = [(i, j)]
+
+                if idx not in clusters:
+                    clusters[idx] = []
+
+                while stk:
+                    ci, cj = stk.pop()
+
+                    seen.add((ci, cj))
+                    points.add((ci, cj))
+
+                    for di, dj in eight_sided_dirs:
+                        ni = ci+di
+                        nj = cj+dj
+                        
+                        if 0 <= ni < m and 0 <= nj < n and data[ni][nj] == idx and (ni, nj) not in seen:
+                            stk.append((ni, nj))
+
+                clusters[idx].append(points)
+
+    return clusters
